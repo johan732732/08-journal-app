17bbf826bbeccaf13e0ccf370b0ccc3010f108f7
Johan Cordoba <johan.cordoba@globant.com>
2024-09-17 22:15:38 -0500

    Build: Initial project structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './styles.css'
+import { JournalApp } from './JournalApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <JournalApp />
   </StrictMode>,
 )
