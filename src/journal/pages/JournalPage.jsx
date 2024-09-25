0f78c6a8fe881f7461953fffed8c97c2c71037b7
Johan Cordoba <johan.cordoba@globant.com>
2024-09-24 20:58:59 -0500

    Style: Create theme component

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,11 @@
+import { MailOutlineOutlined } from '@mui/icons-material';
+import { Typography } from '@mui/material';
+
 export const JournalPage = () => {
-  return <div>JournalPage</div>;
+  return (
+    <>
+      <Typography variant='h1'>JournalPage</Typography>
+      <MailOutlineOutlined />
+    </>
+  );
 };
