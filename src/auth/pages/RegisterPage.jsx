ffa0fdc6eb6ee2ee39c6c7dd73729ddc4cf68d39
Johan Córdoba <johan732732@gmail.com>
2025-04-16 00:29:40 -0500

    Use useForm for register component

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,52 +1,80 @@
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink } from "react-router-dom";
 
-import { Button, Grid2, Link, TextField, Typography } from '@mui/material';
-import { AuthLayout } from '../layout/AuthLayout';
+import { Button, Grid2, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks";
+
+const formData = {
+  email: "johan732732@gmail.com",
+  password: "123456",
+  displayName: "Johan Cordoba",
+};
 
 export const RegisterPage = () => {
+  const { displayName, email, password, onInputChange } = useForm(formData);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log({ displayName, email, password });
+  };
+
   return (
-    <AuthLayout title='Register'>
-      <form>
+    <AuthLayout title="Register">
+      <form onClick={onSubmit}>
         <Grid2 container>
           <Grid2 item size={{ xs: 12 }} sx={{ mt: 2 }}>
             <TextField
-              label='Name'
-              type='text'
-              placeholder='Full Name'
+              label="Name"
+              type="text"
+              placeholder="Full Name"
               fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
             />
           </Grid2>
 
           <Grid2 item size={{ xs: 12 }} sx={{ mt: 2 }}>
             <TextField
-              label='Email'
-              type='email'
-              placeholder='Email'
+              label="Email"
+              type="email"
+              placeholder="Email"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
             />
           </Grid2>
 
           <Grid2 item size={{ xs: 12 }} xs={12} sx={{ mt: 2 }}>
             <TextField
-              label='Password'
-              type='password'
-              placeholder='*****'
+              label="Password"
+              type="password"
+              placeholder="*****"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
           </Grid2>
 
           <Grid2 container size={12} spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid2 item size={{ xs: 12 }}>
-              <Button variant='contained' width='100%' fullWidth={true}>
+              <Button
+                type="submit"
+                variant="contained"
+                width="100%"
+                fullWidth={true}
+              >
                 Create account
               </Button>
             </Grid2>
           </Grid2>
         </Grid2>
 
-        <Grid2 container direction='row' justifyContent='end'>
+        <Grid2 container direction="row" justifyContent="end">
           <Typography sx={{ mr: 1 }}>Do you have an account?</Typography>
-          <Link component={RouterLink} to='/auth/login' color='inherit'>
+          <Link component={RouterLink} to="/auth/login" color="inherit">
             Login
           </Link>
         </Grid2>
