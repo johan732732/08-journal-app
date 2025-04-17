c2c86f76edf6163e765f23634cf0f8bb1e27e02c
Johan Córdoba <johan732732@gmail.com>
2025-04-17 00:39:12 -0500

    Keep the session

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
-import { useSelector } from "react-redux";
 import { CheckingAuth } from "../ui";
+import { useCheckAuth } from "../hooks";
 
 export const AppRouter = () => {
-  const { status } = useSelector((state) => state.auth);
+  const { status } = useCheckAuth();
 
   if (status == "checking") {
     return <CheckingAuth />;
@@ -13,11 +13,13 @@ export const AppRouter = () => {
 
   return (
     <Routes>
-      {/*  Login & Sign in */}
-      <Route path="/auth/*" element={<AuthRoutes />} />
+      {status === "authenticated" ? (
+        <Route path="/*" element={<JournalRoutes />} />
+      ) : (
+        <Route path="/auth/*" element={<AuthRoutes />} />
+      )}
 
-      {/* JournalApp */}
-      <Route path="/*" element={<JournalRoutes />} />
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
   );
 };
